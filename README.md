da18127d84deb6ca65ba7318263616ad90416b43
Felix Krause <krausefx@gmail.com>
2019-04-30 15:49:02 +0200

    Case insensitive usernames and remove contact feature

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ You'll have to setup a few things
 free - available for a call
 stop - mark yourself as unavailable for a call
 newcontact - [name] Add a new contact
+removecontact - [name] Remove an existing contact
 contacts - list all your contacts
 help - print help screen
 ```
--- telegram_handler.rb
+++ telegram_handler.rb
@@ -16,7 +16,7 @@ module StayInTouch
 
     def self.did_receive_message(message:, bot:)
       puts "Received #{message.text}"
-      from_username = message.from.username
+      from_username = message.from.username.downcase
 
       # ANY message we receive, we're gonna remember the mapping of username to Chat ID
       # to be able to text the given person. This is a privacy/spam protection feature, so that
@@ -63,16 +63,15 @@ module StayInTouch
           bot.api.send_message(chat_id: message.chat.id, text: "Call confirmed, please hit the call button to connect with @#{user_to_confirm}")
 
           telegram_id_owner = Database.database[:openChats].where(telegramUser: user_to_confirm).first[:chatId]
-          bot.api.send_message(chat_id: telegram_id_owner, text: "@#{message.from.username} just confirmed the call, you two should connect 🤗")
+          bot.api.send_message(chat_id: telegram_id_owner, text: "@#{from_username} just confirmed the call, you two should connect 🤗")
           
           # now revoke all other messages
           revoke_all_invites(bot: bot, owner: user_to_confirm)
 
-          Database.database[:contacts].where(owner: user_to_confirm, telegramUser: message.from.username).update(lastCall: Time.now)
+          Database.database[:contacts].where(owner: user_to_confirm, telegramUser: from_username).update(lastCall: Time.now)
         when "/contacts"
           to_print = Database.database[:contacts].where(owner: from_username).reverse_order(:lastCall).collect do |row|
             if row[:lastCall]
-              formatted_date = row[:lastCall].strftime("%Y-%m-%d")
               days_since_last_call = ((Time.now - row[:lastCall]) / 60.0 / 60.0 / 24.0).round
 
               emoji = if days_since_last_call > 7
@@ -81,21 +80,20 @@ module StayInTouch
                 "✅"
               end
 
-              formatted_days_ago = " #{days_since_last_call} day" + (formatted_days_ago != 1 ? "s" : "") + " ago"
+              formatted_days_ago = "#{days_since_last_call} day" + (formatted_days_ago != 1 ? "s" : "") + " ago"
             else
-              formatted_date = "Not yet called"
               emoji = "➡"
-              formatted_days_ago = ""
+              formatted_days_ago = "Never"
             end
 
-            "#{emoji}#{formatted_days_ago}: #{row[:telegramUser]}: #{formatted_date}"
+            "#{emoji} #{formatted_days_ago}: @#{row[:telegramUser]}"
           end
           bot.api.send_message(
             chat_id: message.chat.id,
             text: to_print.join("\n")
           )
         when /\/newcontact (.*)/
-          username = message.text.match(/\/newcontact (.*)/)[1].gsub("@", "")
+          username = message.text.match(/\/newcontact (.*)/)[1].gsub("@", "").downcase
           Database.database[:contacts] << {
             lastCall: nil,
             owner: from_username,
@@ -105,6 +103,16 @@ module StayInTouch
 
           if Database.database[:openChats].where(telegramUser: username).count == 0
             send_invite_text(bot: bot, chat_id: message.chat.id, from: from_username, to: username)
+          end
+        when /\/removecontact (.*)/
+          username = message.text.match(/\/removecontact (.*)/)[1].gsub("@", "").downcase
+
+          contacts = Database.database[:contacts].where(owner: from_username, telegramUser: username)
+          if contacts.count == 1
+            contacts.delete
+            bot.api.send_message(chat_id: message.chat.id, text: "Successfully removed @#{username} from your contact list")
+          else
+            bot.api.send_message(chat_id: message.chat.id, text: "Could not find contact named @#{username}, please run /contacts for a list of your contacts")
           end
         when "/newcontact"
           bot.api.send_message(chat_id: message.chat.id, text: "Please enter `/newcontact [username]`")
